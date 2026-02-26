2a21cf6b08bf601925b6efc8ca5db6dad869c208
mhdrahman <mhd.rahman601@gmail.com>
2026-02-26 14:03:25 +0000

    Start adding tests, cover normal case

--- GildedRoseTests/GildedRoseTests.cs
+++ GildedRoseTests/GildedRoseTests.cs
@@ -7,11 +7,35 @@ namespace GildedRoseTests;
 public class GildedRoseTests
 {
     [Fact]
-    public void ExampleTest()
+    public void NormalItem_DecreasesQualityAndSellIn_LimitsAndRulesRespected()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
-        app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        // Arrange
+        var items = new List<Item>
+        {
+            // Item before sell by
+            new() { Name = "foo", SellIn = 5, Quality = 10 },
+            
+            // Item past sell by - should degrade at double speed
+            new() { Name = "foo", SellIn = 0, Quality = 5 },
+        };
+        
+        // Act
+        RunApp(items, days: 5);
+        
+        // Assert
+        Assert.Equal(5, items[0].Quality);
+        Assert.Equal(0, items[0].SellIn);
+        
+        Assert.Equal(0, items[1].Quality);
+        Assert.Equal(-5, items[1].SellIn);
+    }
+
+    private static void RunApp(List<Item> items, int days = 1)
+    {
+        var app = new GildedRose(items);
+        for (var i = 0; i < days; i++)
+        {
+            app.UpdateQuality();
+        }
     }
 }
