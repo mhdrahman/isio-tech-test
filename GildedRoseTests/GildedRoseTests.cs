2e0a1303f62abcd4e7f6b2ad0f5376d3901b7866
mhdrahman <mhd.rahman601@gmail.com>
2026-02-26 14:14:02 +0000

    Add tests for aged brie and sulfuras

--- GildedRoseTests/GildedRoseTests.cs
+++ GildedRoseTests/GildedRoseTests.cs
@@ -1,13 +1,14 @@
 ﻿using Xunit;
 using System.Collections.Generic;
 using GildedRoseKata;
+using GildedRoseKata.Helpers;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTests
 {
     [Fact]
-    public void NormalItem_DecreasesQualityAndSellIn_LimitsAndRulesRespected()
+    public void NormalItem_RulesRespected()
     {
         // Arrange
         var items = new List<Item>
@@ -28,6 +29,60 @@ public class GildedRoseTests
         
         Assert.Equal(0, items[1].Quality);
         Assert.Equal(-5, items[1].SellIn);
+    }
+
+    [Fact]
+    public void AgedBrie_RulesRespected()
+    {
+        // Arrange
+        var items = new List<Item>
+        {
+            // Item before sell by - quality should increase by 1 per day
+            new() { Name = Constants.AgedBrie, SellIn = 5, Quality = 10 },
+            
+            // Item past sell by - quality should increase by 2 per day
+            new() { Name = Constants.AgedBrie, SellIn = 0, Quality = 10 },
+            
+            // Quality should be capped at 40
+            new() { Name = Constants.AgedBrie, SellIn = 0, Quality = 39 },
+        };
+        
+        // Act
+        RunApp(items, days: 1);
+        
+        // Assert
+        Assert.Equal(11, items[0].Quality);
+        Assert.Equal(4, items[0].SellIn);
+        
+        Assert.Equal(12, items[1].Quality);
+        Assert.Equal(-1, items[1].SellIn);
+        
+        Assert.Equal(40, items[2].Quality);
+        Assert.Equal(-1, items[2].SellIn);
+    }
+
+    [Fact]
+    public void Sulfuras_RulesRespected()
+    {
+        // Arrange
+        var items = new List<Item>
+        {
+            // Item before sell by with negative quality - nothing should change
+            new() { Name = Constants.Sulfuras, SellIn = 5, Quality = -10 },
+            
+            // Item after sell by - nothing should change
+            new() { Name = Constants.Sulfuras, SellIn = 0, Quality = 50 },
+        };
+        
+        // Act
+        RunApp(items, days: 3);
+        
+        // Assert
+        Assert.Equal(-10, items[0].Quality);
+        Assert.Equal(5, items[0].SellIn);
+        
+        Assert.Equal(50, items[1].Quality);
+        Assert.Equal(0, items[1].SellIn);
     }
 
     private static void RunApp(List<Item> items, int days = 1)
