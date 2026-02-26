421504fa01f2640f2aa057fa25cb92c4f5f7276a
mhdrahman <mhd.rahman601@gmail.com>
2026-02-26 12:10:32 +0000

    Initial commit

---
+++ GildedRoseTests/GildedRoseTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTests
+{
+    [Fact]
+    public void ExampleTest()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("fixme", Items[0].Name);
+    }
+}
